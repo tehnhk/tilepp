91708a0aa49982c911d9f6308655197144814eeb
kirixon <kirix820@gmail.com>
2013-03-23 18:56:46 +0700

    coordinates fix

--- drawer.cpp
+++ drawer.cpp
@@ -13,15 +13,16 @@ void Display()
   glClear(GL_COLOR_BUFFER_BIT);
   for (int i=0;i<10;i++)    //Начинаем рисовать чушь
   {
-    glColor3f(0.5,0.5,0.5); //Серый цвет
+    glColor3f(1,0.5,0.5); //Серый цвет
     glBegin(GL_QUADS);
-    glVertex2f(i*20,i*20);
-    glVertex2f(i*20,(i*20)+20);
-    glVertex2f((i*20)+20,(i*20)+20);
-    glVertex2f((i*20)+20,i*20);
+    glVertex2f(i*2,i*2);
+    glVertex2f(i*2,(i*2)+2);
+    glVertex2f((i*2)+2,(i*2)+2);
+    glVertex2f((i*2)+2,i*2);
     glEnd();
   }
   glutSwapBuffers();
+  glFlush();
 }
 
 
@@ -32,11 +33,13 @@ void Drawer::Init(int argc,char **argv)
   glutInit(&argc,argv);
   glutInitWindowSize(W,H);
   glutInitWindowPosition(200,200);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-  glOrtho(0,800,600,0,-1.0,1.0);
+  glutInitDisplayMode(GLUT_RGB);
+  glutCreateWindow("Tile++");
+  glViewport(0,0,W,H);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  glutCreateWindow("Tile++");
+  gluOrtho2D(0,W,H,0);
+  glClearColor(0,0,0,0);
   glutDisplayFunc(Display);
   glutMainLoop();
 }
