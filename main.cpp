525fb86860f5197b8b0d8ec36a59c36bb4aad31d
A. T. Thordendal <thordendal@thordendal.ru>
2013-03-23 23:48:59 +0700

    cleaned out test things

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 Game NewGame;
 
 int main(int argc, char **argv) {
-  std::cout << argv[1] << argc << std::endl;
+  //std::cout << argv[1] << argc << std::endl;
+  //that was just check for arguments count
   NewGame.Init(argc,argv);
   return 0;
 }
