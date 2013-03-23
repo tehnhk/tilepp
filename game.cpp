e94e8bcbe63b2c6803dfb7f5c5b08dd6b74e7d3a
A. T. Thordendal <thordendal@thordendal.ru>
2013-03-23 23:49:36 +0700

    cleaned out test things

--- game.cpp
+++ game.cpp
@@ -4,5 +4,5 @@ Drawer NewDrawer;
 void Game::Init(int argc, char **argv){
   if ((argc>1) && ( ( strcmp(argv[1],"-g") == 0  ) || (strcmp(argv[1],"--grid")==0) ) )
     NewDrawer.SetGrid(false);  
-NewDrawer.Init(argc,argv);
+  NewDrawer.Init(argc,argv);
 }
