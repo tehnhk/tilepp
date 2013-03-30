b75a20143054ad0b54965604f11f94000f23b97b
kirixon <kirix820@gmail.com>
2013-03-30 22:52:49 +0600

    FUCK

--- README.md
+++
@@ -1,3 +0,0 @@
-Tile++
-======
-<b>Tile++</b> (it's just working name) is 1,5-men-project of simple 2d-survival shooter written in C++ without using any of popular game engines.
--- game.cpp
+++
@@ -1,8 +0,0 @@
-#include "game.h"
-#include <string.h>
-Drawer NewDrawer;
-void Game::Init(int argc, char **argv){
-  if ((argc>1) && ( ( strcmp(argv[1],"-g") == 0  ) || (strcmp(argv[1],"--grid")==0) ) )
-    NewDrawer.SetGrid(false);  
-  NewDrawer.Init(argc,argv);
-}
